b7dd15045c4a356f319e0aff5195137683ce0c1d
VcSaJen <VcSaJen@ya.ru>
2016-03-13 13:58:10 +0900

    Commented code cleanup
    Change e-mail notification for travis

--- src/main/java/com/vcsajen/bbcodeforsponge/BBCodeForSponge.java
+++ src/main/java/com/vcsajen/bbcodeforsponge/BBCodeForSponge.java
@@ -49,17 +49,6 @@ public class BBCodeForSponge {
     @Inject
     PluginContainer plugin;
 
-    /*@Listener
-    public void onMessageEvent(MessageEvent event, @First Player player){
-        if (!player.hasPermission("bbcode")) return;
-        if (!event.getMessage().isPresent()) return;
-        String msg = event.getOriginalMessage().get().toPlain();
-
-        msg = "Эм, "+msg+", мда.";
-
-        event.setMessage(Text.of(msg));
-    }*/
-
     private Map<String, Set<Code>> permissableCodes;
     private Set<Code> allPermCodes;
     private Configuration bbConfiguration;
@@ -174,34 +163,16 @@ public class BBCodeForSponge {
 
     @Listener(order = Order.LATE)
     public void chatEvent(final MessageChannelEvent.Chat chat, @First final Player player){
-        //if (!player.hasPermission("bbcodeforsponge.use")) return;
+        if (!player.hasPermission("bbcodeforsponge.use")) return;
 
-        /*Text mytext = chat.getFormatter().getBody().format();
-        for (Text t: mytext.withChildren()) {
-            player.sendMessage(Text.of("Content: "+((LiteralText)t).getContent()));
-            //player.sendMessage(t);
-        }*/
-
-        long start = System.nanoTime();
-        //--------------------------
         boolean playingNice;
 
-        /*logger.debug("Header: ");
-        chat.getFormatter().getHeader().getAll().forEach(t -> {logger.debug(t.getClass().getName() + " => " + t.getParameters().toString());});
-
-        logger.debug("Body: ");
-        chat.getFormatter().getBody().getAll().forEach(t -> {logger.debug(t.getClass().getName() + " => " + t.getParameters().toString());});
-
-        logger.debug("Footer: ");
-        chat.getFormatter().getFooter().getAll().forEach(t -> {logger.debug(t.getClass().getName() + " => " + t.getParameters().toString());});
-*/
         MutableObject<Text> msg_ = new MutableObject<>();
         chat.getFormatter().getBody().forEach(MessageEvent.DefaultBodyApplier.class, applier -> msg_.setValue(Text.of(applier.getParameter("body"))));
 
-
         String msg;
         Text header = Text.EMPTY;
-        //String msg = chat.getFormatter().getBody().format().toPlain();
+
         playingNice = msg_.getValue()!=null;
         if (playingNice)
             msg = msg_.getValue().toPlain(); //Nice
@@ -213,26 +184,7 @@ public class BBCodeForSponge {
             else msg = chat.getRawMessage().toPlain(); //Brute
         }
 
-        /*String msg1 = chat.getMessage().toPlain();
-        String msg2 = chat.getOriginalMessage().toPlain();
-        String msg3 = chat.getRawMessage().toString();
-        player.sendMessage(Text.of("Message: ", msg, "; OriginalMessage: ", msg2, "; RawMessage: ", msg3));*/
-
-        //msg = "Эм, "+msg+", мда.";
-
-        msg = formatWithBBCode(msg, player);
-
-
-        //Text t = TextSerializers.JSON.deserialize("{text:\"Hover.\",hoverEvent:{action:show_text,value:\"Hello\\nthere.\"}}");
-
-        //String s = TextSerializers.TEXT_XML.serialize(t);
-
-        //player.sendMessage(Text.of(s));
-        //player.sendMessage(TextSerializers.TEXT_XML.deserialize(s));
-
-        //player.sendMessage(Text.of(msg));
-
-        //chat.setMessage(TextSerializers.TEXT_XML.deserialize("&lt;"+player.getName()+"&gt; "+msg));
+        msg = formatWithBBCode(msg, player); /*<<<<<<<*/
 
         Text formattedMsg = TextSerializers.TEXT_XML.deserialize(msg);
 
@@ -244,11 +196,6 @@ public class BBCodeForSponge {
             if (header.isEmpty())
                 logger.warn("Some of installed chat plugins doesn't support new MessageEvent appliers! Manual extracting of header failed! Things can be broken.");
         }
-
-        long end = System.nanoTime();
-        logger.debug(Long.toString(end-start));
-        //chat.setMessage(TextSerializers.TEXT_XML.deserialize("&lt;"+player.getName()+"&gt; <span onHover=\"show_text('&lt;u&gt;LOL&#xD;&#xA;LOL&lt;/u&gt;')\">[+++]</span>"));
-        //chat.setMessage(t);
     }
 
     @Listener
